cf78330fab349c0181885a53d827ee9693785f61
Tulio Goulart <tulio.gp2@gmail.com>
2021-10-18 20:56:48 +0200

    fix App.js bug fixed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import { GlobalStyles } from "./Styles/global";
-import MyGroups from "./Pages/MyGroups/MyGroups";
-import NewGroup from "./Components/NewGroup";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyles />
-      <MyGroups />
-      <NewGroup />
     </div>
   );
 }
