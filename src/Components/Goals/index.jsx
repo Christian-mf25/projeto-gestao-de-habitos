e20b49878e9118efc1d21bab4a328442a9252427
Maria <mfb171201@gmail.com>
2021-10-20 19:40:50 -0400

    feature/InfoGroups -> update goal

--- src/Components/Goals/index.jsx
+++ src/Components/Goals/index.jsx
@@ -1,15 +1,40 @@
 import {useEffect, useState} from "react";
 import Api from "../../Services/API"
 import {useParams} from "react-router-dom";
+import { toast } from "react-toastify";
 
 const Goals = () => {
-    const {id} = useParams()
-    const [goals, setGoals] = useState([])
+    const {id} = useParams();
+    const [goals, setGoals] = useState([]);
+    const token = JSON.parse(localStorage.getItem("@Productive:token"));
+    const checkIn = (goal) => {
+        if (goal.how_much_achieved < 50) {
+          goal.how_much_achieved += 1;
+        } else {
+          goal.achieved = true;
+        }
+        Api.patch(`/goals/${goal.id}/`,
+          {
+            how_much_achieved: goal.how_much_achieved,
+            achieved: true,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        ).then(() => {
+          toast.success("Check-in realizado!");
+        });
+    };
+
     useEffect(() => {
         Api.get(`goals/?group=${id}&page=1`)
           .then((response) => setGoals(response.data.results))
           .catch((err) => console.log(err));
     });
+
     return (
         <ul>
             {goals
@@ -19,6 +44,7 @@ const Goals = () => {
                     <p>{goal.difficulty}</p>
                     <p>{goal.achieved}</p>
                     <span>{goal.how_much_achieved}</span>
+                    <button onClick={() => checkIn(goal)}>Check-in</button>
                 </li>)}
         </ul>
     );
