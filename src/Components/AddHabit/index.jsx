1ba7e88af8246ee7499ef7005b670ccda6b1146b
Julio Marodin <julio.marodin@gmail.com>
2021-10-21 00:52:23 -0300

    feat: a bit more styled

--- src/Components/AddHabit/index.jsx
+++ src/Components/AddHabit/index.jsx
@@ -1,13 +1,153 @@
 import "./styles.css";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import Api from "../../Services/API";
+import { toast } from "react-toastify";
+import { TextField } from "@material-ui/core";
+import jwtDecode from "jwt-decode";
+import { useState } from "react";
 
 const AddHabit = (props) => {
+  const [value, setValue] = useState();
+  const token = JSON.parse(localStorage.getItem("@Productive:token"));
+  let userId = "";
+  if (token) {
+    userId = jwtDecode(token, { payload: true });
+  }
+
+  const schema = yup
+    .object()
+    .shape({ title: yup.string().required("Campo obrigatório") });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) });
+
+  const submitForm = ({ title, category, difficulty, frequency }) => {
+    Api.post(
+      "habits/",
+      {
+        title: title,
+        category: category,
+        difficulty: difficulty,
+        frequency: frequency,
+        achieved: false,
+        how_much_achieved: 0,
+        user: userId.user_id,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    ).then(
+      (_) => (
+        // eslint-disable-next-line no-sequences
+        toast.success("Hábito adicionado com sucesso!"),
+        props.setButtonPopup(!props.buttonPopup),
+        update()
+      )
+    );
+  };
+
+  const update = () => {
+    setValue({});
+  };
+
   return props.trigger ? (
     <div className="popup">
       <div className="popup-inner">
-        <button onClick={() => props.setTrigger(false)} className="cancel-btn">
-          Cancelar
-        </button>
+        <div className="popup-header">
+          <h3>Criar novo hábito</h3>
+          <button
+            onClick={() => props.setTrigger(false)}
+            className="cancel-btn"
+          >
+            X
+          </button>
+        </div>
         {props.children}
+        <form className="radioContainer" onSubmit={handleSubmit(submitForm)}>
+          <TextField
+            label="Title"
+            margin="normal"
+            variant="outlined"
+            size="small"
+            color="primary"
+            {...register("title")}
+            error={!!errors.title}
+            helperText={errors.title?.message}
+          />
+          <div className="radioOptions">
+            <div className="toggle">
+              <p>Categoria</p>
+              <div>
+                <input type="radio" value="Saúde" />
+                <label>Saúde</label>
+              </div>
+              <div>
+                <input type="radio" value="Educação" />
+                <label>Educação</label>
+              </div>
+              <div>
+                <input type="radio" value="Meditação" />
+                <label>Meditação</label>
+              </div>
+              <div>
+                <input type="radio" value="Lazer" />
+                <label>Lazer</label>
+              </div>
+              <div>
+                <input type="radio" value="Outro" />
+                <label>Outro</label>
+              </div>
+            </div>
+            <div className="toggle">
+              <p>Dificuldade</p>
+              <div>
+                <input type="radio" value="very_easy" />
+                <label>Very Easy</label>
+              </div>
+              <div>
+                <input type="radio" value="easy" />
+                <label>Easy</label>
+              </div>
+              <div>
+                <input type="radio" value="medium" />
+                <label>Medium</label>
+              </div>
+              <div>
+                <input type="radio" value="hard" />
+                <label>Hard</label>
+              </div>
+              <div>
+                <input type="radio" value="very_hard" />
+                <label>Very Hard</label>
+              </div>
+            </div>
+            <div className="toggle">
+              <p>Com qual frequência?</p>
+              <div>
+                <input type="radio" value="Diário" />
+                <label>Diário</label>
+              </div>
+              <div>
+                <input type="radio" value="Semanal" />
+                <label>Semanal</label>
+              </div>
+              <div>
+                <input type="radio" value="Mensal" />
+                <label>Mensal</label>
+              </div>
+            </div>
+          </div>
+          <button type="submit" variant="contained" color="primary">
+            Adicionar
+          </button>
+        </form>
       </div>
     </div>
   ) : (
--- src/Components/SearchHabit/index.jsx
+++ src/Components/SearchHabit/index.jsx
@@ -1,47 +1,9 @@
 import { TextField } from "@material-ui/core";
 import { useState } from "react";
-import { toast } from "react-toastify";
-import Api from "../../Services/API";
 import CardHabit from "../CardHabit";
 
-const SearchHabit = ({ habitsRes, setIsSearching, getHabits }) => {
+const SearchHabit = ({ habitsRes, setIsSearching }) => {
   const [userInput, setUserInput] = useState("");
-  const token = JSON.parse(localStorage.getItem("@Productive:token"));
-
-  const checkIn = (item) => {
-    if (item.how_much_achieved < 30) {
-      item.how_much_achieved += 1;
-    } else {
-      item.achieved = true;
-    }
-    Api.patch(
-      `habits/${item.id}/`,
-      {
-        how_much_achieved: item.how_much_achieved,
-        achieved: item.achieved,
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    ).then(() => {
-      toast.success("Check-in realizado!");
-      getHabits();
-    });
-  };
-
-  const deleteHabit = (id) => {
-    Api.delete(`habits/${id}/`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    }).then(() => {
-      toast.success("Hábito removido");
-      getHabits();
-    });
-  };
 
   return (
     <div>
@@ -64,17 +26,9 @@ const SearchHabit = ({ habitsRes, setIsSearching, getHabits }) => {
                 item.title.toLowerCase().includes(userInput.toLowerCase())
               )
               .map((filter, index) => (
-                <li key={index}>
-                  <h3>{filter.title}</h3>
-                  <p>{filter.category}</p>
-                  <span>{filter.difficulty}</span>
-                  <p>{filter.frequency}</p>
-                  <p>{filter.how_much_achieved}</p>
-                  <button onClick={() => checkIn(filter)}>Check-in</button>
-                  <button onClick={() => deleteHabit(filter.id)}>
-                    Deletar
-                  </button>
-                </li>
+                <div key={index}>
+                  <CardHabit item={filter} />
+                </div>
               )))
           : habitsRes?.map((item, index) => (
               <div key={index}>
