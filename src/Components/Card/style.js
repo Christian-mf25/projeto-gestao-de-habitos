50c6f5682b8ac9f9f3f22b3fffbc130b0818a51f
Christian <christian.mf22@gmail.com>
2021-10-22 09:34:05 -0300

    style: fixed style at cardgroup

--- src/Components/Card/style.js
+++ src/Components/Card/style.js
@@ -32,11 +32,7 @@ export const ContainerEditGroupCard = styled.div`
     width: 20px;
     height: 20px;
     position: absolute;
-<<<<<<< HEAD
     right: 10px;
-=======
-    right: 12%;
->>>>>>> 2626084793093a2a22c8c36383a99909cf61a5b3
     margin-top: 20px;
     background-color: transparent;
     border: none;
@@ -45,44 +41,4 @@ export const ContainerEditGroupCard = styled.div`
   button:hover {
     cursor: pointer;
   }
-
-<<<<<<< HEAD
-  
-=======
-  @media (min-width: 425px) {
-    button {
-      right: 16%;
-    }
-  }
-
-  @media (min-width: 473px) {
-    button {
-      right: 18%;
-    }
-  }
-
-  @media (min-width: 768px) {
-    button {
-      right: 28%;
-    }
-  }
-
-  @media (min-width: 1024px) {
-    button {
-      right: 34%;
-    }
-  }
-
-  @media (min-width: 1040px) {
-    button {
-      right: 38%;
-    }
-  }
-
-  @media (min-width: 2560px) {
-    button {
-      right: 43%;
-    }
-  }
->>>>>>> 2626084793093a2a22c8c36383a99909cf61a5b3
 `;
