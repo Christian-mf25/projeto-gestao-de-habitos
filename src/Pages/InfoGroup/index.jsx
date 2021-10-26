28b1044ff754c988e79db7d8e7522c03331f267a
Maria <mfb171201@gmail.com>
2021-10-25 23:15:42 -0400

    feature/InfoGroup -> informações adicionais

--- src/Pages/InfoGroup/index.jsx
+++ src/Pages/InfoGroup/index.jsx
@@ -1,17 +1,45 @@
-import Goals from "../../Components/Goals"
-import NewGoal from "../../Components/NewGoal";
-import Activities from "../../Components/Activities"
+import Goals from "../../Components/Goals";
+import Activities from "../../Components/Activities";
 import NewActivity from "../../Components/NewActivity";
+import Header from "../../Components/Header";
+import { CardActivity } from "./style";
+import { useParams } from "react-router-dom";
+import { useContext } from "react";
+import { GroupContext } from "../../Providers/Group";
 
 const InfoGroup = () => {
-    return (
-        <>
-            <NewGoal/>
-            <Goals/>
-            <NewActivity/>
-            <Activities/>
-        </>
-    );
-}
+  const { id } = useParams();
+  const { data } = useContext(GroupContext);
+
+  return (
+    <>
+      <Header showM />
+      <ul>
+      {data
+      .filter(info => info.id == id)
+      .map(filt => (
+        <li key={filt.id}>
+          {filt.name}
+          {filt.creator.username}
+          Categoria: {filt.category}
+          Participantes: {filt.users_on_group.length}
+          Metas: {filt.goals.length}
+          Descrição: {filt.description}
+          Aliados: {filt.users_on_group
+          .map(user => user.username)}
+        </li>
+      ))}
+      </ul>
+
+
+      <Goals />
+
+      <CardActivity>
+        <NewActivity />
+        <Activities />
+      </CardActivity>
+    </>
+  );
+};
 
 export default InfoGroup;
