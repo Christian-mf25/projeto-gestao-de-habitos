fa50208682395ac7cbe860c3fdb8bfced6382453
Maria <mfb171201@gmail.com>
2021-10-26 11:08:07 -0400

    feature/InfoGroup -> botão sair/editar de acordo com o usuário logado

--- src/Pages/InfoGroup/index.jsx
+++ src/Pages/InfoGroup/index.jsx
@@ -4,12 +4,35 @@ import NewActivity from "../../Components/NewActivity";
 import Header from "../../Components/Header";
 import { CardActivity } from "./style";
 import { useParams } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { GroupContext } from "../../Providers/Group";
+import jwt from "jwt-decode";
+import Api from "../../Services/API";
+import { toast } from "react-toastify";
+import {ContainerEditGroupCard, DivCard} from "../../Components/Card/style";
+import {EditGroupCard} from "../../Components/EditGroup"
 
 const InfoGroup = () => {
   const { id } = useParams();
   const { data } = useContext(GroupContext);
+  const token = JSON.parse(localStorage.getItem("@Productive:token"));
+  const userId = jwt(token);
+  const [actived, setActived] = useState(false);
+  
+  const deleteGroup = (filt) => {
+    Api.delete(`/groups/${filt.id}/unsubscribe/`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then(() => {
+        toast.success("Grupo Apagado!");
+      })
+      .catch((err) => console.log("erro", err));
+
+    setActived(false);
+  };
 
   return (
     <>
@@ -20,7 +43,22 @@ const InfoGroup = () => {
       .map(filt => (
         <li key={filt.id}>
           {filt.name}
-          {filt.creator.username}
+          {filt.creator.id === userId.user_id 
+          ?
+          <ContainerEditGroupCard>
+            {actived && (
+            <EditGroupCard
+            setActived={setActived}
+            key={filt.id}
+            actived={actived}
+            item={filt}
+          />
+            )}
+          <button className="buttonX" onClick={() => setActived(true)}></button>
+          </ContainerEditGroupCard>
+          : 
+          <button onClick={() =>deleteGroup(filt)}>SAIR</button>
+          }
           Categoria: {filt.category}
           Participantes: {filt.users_on_group.length}
           Metas: {filt.goals.length}
